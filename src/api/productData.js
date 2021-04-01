7daf57ad0ea7fbadd0feec966053054bf4c4edab
Varen Aggarwal <varenaggarwal@gmail.com>
2021-04-01 12:51:16 +0530

    add faker random data

--- src/api/productData.js
+++ src/api/productData.js
@@ -1,40 +1,14 @@
-export const productData = [
-  {
-    id: 1,
-    name: "Amplitude workshop",
-    img: "https://inc42.com/wp-content/uploads/2020/10/Artboard-1-100-1-1.jpg",
-    date: "",
-    location: "Online",
-    price: 1000,
-    wishlist: false,
-    cartQty: 0,
-    inStock: true,
-    fastDelivery: true,
-  },
-  {
-    id: 2,
-    name: "ETF Masterclass | The Future of Investing",
-    img:
-      "https://res.cloudinary.com/dwzmsvp7f/image/fetch/q_75,f_auto,w_800/https%3A%2F%2Fmedia.insider.in%2Fimage%2Fupload%2Fc_crop%2Cg_custom%2Fv1616299154%2Fymfr76sjtbjpnzt9uc3f.png",
-    date: "",
-    location: "Online",
-    price: 999,
-    wishlist: false,
-    cartQty: 0,
-    inStock: false,
-    fastDelivery: true,
-  },
-  {
-    id: 3,
-    name: "Kommune presents Mulaqat: An Introductory Workshop of Urdu Ghazal",
-    img:
-      "https://res.cloudinary.com/dwzmsvp7f/image/fetch/q_75,f_auto,w_800/https%3A%2F%2Fmedia.insider.in%2Fimage%2Fupload%2Fc_crop%2Cg_custom%2Fv1615622633%2Fdjezshxcmfxzcn8lwzbs.jpg",
-    date: "",
-    location: "Online",
-    price: 499,
-    wishlist: false,
-    cartQty: 0,
-    inStock: true,
-    fastDelivery: false,
-  },
-];
+import faker from "faker";
+
+export const productData = [...Array(25)].map(() => ({
+  id: faker.datatype.uuid(),
+  name: faker.commerce.productName(),
+  img: faker.image.image(),
+  date: "",
+  location: "Online",
+  price: faker.commerce.price(),
+  wishlist: false,
+  cartQty: 0,
+  inStock: faker.datatype.boolean(),
+  fastDelivery: faker.datatype.boolean(),
+}));
